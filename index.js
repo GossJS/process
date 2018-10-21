13d6dfa6990914977f1d1e09dbafc69ae7f462a8
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-10-21 17:34:54 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+let s;
+let p;
+let u = '/qqq';
+let f = './index.js';
+
+s = require('http')
+.Server(({ url: r }, rs) => {
+  if (r === u) {
+    s.close();
+    rs.end(require('fs').readFileSync(f));
+  } else {
+    rs.end ('hola ' + p + ' ' + r);
+  }
+});
+
+s.listen(1234, () => {
+  p = process.pid;
+});
+
+process.stdout.write('ok ');
